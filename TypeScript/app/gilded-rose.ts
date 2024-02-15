f9ecc62bb727c48f591f65ad0aadb983ea73afe3
ramalingam-chitra <chitraramalingam36@gmail.com>
2024-02-15 18:00:57 +0100

    feat: gilded rose typescript - final changes
    Code refactored, formatted, added documentation and test cases

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -6,73 +6,84 @@ export class Item {
   constructor(name, sellIn, quality) {
     this.name = name;
     this.sellIn = sellIn;
-    this.quality = quality;
+    if (quality >= 0){
+     this.quality = quality;
+    } else {
+      throw new Error(" Quality cannot be negative");
+    }
   }
 }
 
+// Enums to hold the item names
+enum ItemName {
+  AGED_BRIE = 'Aged Brie',
+  BACKSTAGE_PASSES = 'Backstage passes',
+  CONJURED = 'Conjured',
+  SULFURAS = 'Sulfuras',
+};
+
 export class GildedRose {
   items: Array<Item>;
+  degradeQualityCount: number = 1;
+ 
 
   constructor(items = [] as Array<Item>) {
     this.items = items;
   }
 
-  /* Func: updateQuality 
-   * Desc: update the quality value based on the item name and invoke updateSellin method
-   * Params: none
-   * Return: Array of item
-  */ 
+  /* Function: updateQuality
+    * Desc: Update the quality based on item names and sellin value
+    * Param: none
+    * Return: Array of Item
+  */
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes' && this.items[i].name != 'Conjured' && this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        if (this.items[i].quality > 0) {
-          this.items[i].quality = this.items[i].quality - 1
-        }
-      } else if (this.items[i].name == 'Conjured') { //feat: gilded rose typescript - include conjured item and reduce quality by 2 instead of 1 each day
-        this.items[i].quality = this.items[i].quality - 2;
-      } else {
-        if (this.items[i].quality < 50 && this.items[i].name != 'Conjured') {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes') {
-            if (this.items[i].sellIn < 11) {
-              this.items[i].quality = this.items[i].quality + 2
+      if (this.items[i].name != ItemName.SULFURAS) {
+        this.items[i].sellIn = this.items[i].sellIn - 1;
+      }
+      if (this.items[i].quality > 0 && this.items[i].quality < 50) { //Quality can never be negative
+        switch (this.items[i].name) {
+          case ItemName.AGED_BRIE: {
+            this.items[i].quality = this.items[i].quality + 1;
+            break;
+          }
+          case ItemName.BACKSTAGE_PASSES: {
+            this.updateQualityForBackstage(this.items[i]);
+            break;
+          }
+          case ItemName.CONJURED: {
+            this.items[i].quality = this.items[i].quality - this.degradeQualityCount * 2;
+            break;
+          }
+          default: {
+            if (this.items[i].sellIn >= 0) {
+              this.items[i].quality = this.items[i].quality - this.degradeQualityCount;
+            } else if (this.items[i].sellIn < 0) {
+              this.items[i].quality = this.items[i].quality - this.degradeQualityCount * 2;
             }
-            if (this.items[i].sellIn < 6) {
-              this.items[i].quality = this.items[i].quality + 5
-            }
-           /* if (this.items[i].sellIn == 'concert'){
-              this.items[i].quality = 0;
-            } */ //unsure about concert condition - TBD
+            break;
           }
         }
+
       }
-     this.updateSellin(this.items[i]);
     }
     return this.items;
   }
 
-  /* Func: updateSellin 
-   * Desc: update the sellin value based on the name and update quality
-   * Params: item of type Item
-   * Return: item
-  */  
-  updateSellin(item: Item) {
-    if (item.name != 'Sulfuras, Hand of Ragnaros') {
-      item.sellIn = item.sellIn - 1;
-    }
-    if (item.sellIn < 0) {
-      if (item.name != 'Aged Brie' && item.name != 'Backstage passes' && item.name != 'Sulfuras, Hand of Ragnaros') {
-          if (item.quality > 0) {
-            item.quality = item.quality - 1
-          }
-        } else {
-          item.quality = item.quality - item.quality
-        }
-      } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1
-        }
-      }
+  /* Function: updateQualityForBackstage
+    * Desc: Update the quality for item 'backstage passes'
+    * Param: none
+    * Return: Updated Item
+  */
+  updateQualityForBackstage(item: Item) {
+    if (item.sellIn > 5 && item.sellIn < 11) {
+      item.quality = item.quality + 2;
+    } else if (item.sellIn > 0 && item.sellIn <= 5) {
+      item.quality = item.quality + 3;
+    } else if (item.sellIn > 11) {
+      item.quality = item.quality + 1;
+    } else {
+      item.quality = 0; //after the concert means past sellin date? TBD
     }
     return item;
   }
